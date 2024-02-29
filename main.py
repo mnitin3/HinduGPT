636fc63e17b173d771fe89d0c4d6119392c7b699
Nitin Agarwal <mnitin3@gmail.com>
2024-02-29 11:09:17 +0530

    Update main.py
    included logo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 def get_response(user_input):
     # Simple chatbot responses
     responses = {
-        "hello": "Hello! How can I assist you today?",
+        "hello": "Pranam! How can I assist you today?",
         "how are you": "I'm just a program, but I'm here to help!",
         "thank you": "You're welcome!",
         # Add more responses as needed
@@ -17,14 +17,18 @@ def get_response(user_input):
         return "I'm sorry, I don't understand that question."
 
 # Streamlit UI components
-st.title("HinduGPT")
+st.title("HinduGPT Bot")
+header_container = st.container()
+header_container.image("/images/swastiklogo.png", width=50, height=50)  # Replace "your_logo.png" with the path to your logo image
+header_container.write("Welcome to Hindu History Chatbot")
+
 st.sidebar.title("Navigation")
 
 # Website navigation links
-page = st.sidebar.radio("Go to", ["Home", "About"])
+page = st.sidebar.radio("Go to", ["Home", "Explore"])
 
 if page == "Home":
-    st.header("Welcome to HinduGPT")
+    st.header("Welcome to HinduGPT bot.")
     user_question = st.text_input("Ask me anything:")
     if user_question:
         bot_response = get_response(user_question)
@@ -36,4 +40,4 @@ elif page == "About":
 
 # Footer with GitHub link
 st.sidebar.markdown("---")
-st.sidebar.markdown("[GitHub](https://github.com/mnitin3/HinduGPT)")
+st.sidebar.markdown("[GitHub](https://github.com/yourusername/yourrepository)")
